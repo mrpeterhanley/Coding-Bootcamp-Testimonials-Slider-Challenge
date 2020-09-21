ed4f4baf42a0059e319cf2e23fc1a9cf52f17f18
Peter Hanley <hanley.peter@gmail.com>
2020-09-21 11:48:01 +1000

    First commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const sliderButtonPrev = document.querySelector(".slider__item__prev");
+const sliderButtonNext = document.querySelector(".slider__item__next");
+const textOne = document.querySelector(".text-one");
+const textTwo = document.querySelector(".text-two");
+const photoOne = document.querySelector(".photo-one");
+const photoTwo = document.querySelector(".photo-two");
+
+sliderButtonPrev.addEventListener("click", function () {
+  textOne.classList.toggle("slide-left");
+  textTwo.classList.toggle("slide-right");
+  photoOne.classList.toggle("slide-left");
+  photoTwo.classList.toggle("slide-right");
+});
+
+sliderButtonNext.addEventListener("click", function () {
+  textOne.classList.toggle("slide-left");
+  textTwo.classList.toggle("slide-right");
+  photoOne.classList.toggle("slide-left");
+  photoTwo.classList.toggle("slide-right");
+});
